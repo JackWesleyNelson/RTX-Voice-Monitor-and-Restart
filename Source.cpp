8d90cfb49ad9fac8f7f0a80097bf3c3ca5282b30
JackWesleyNelson <Administrator@WIN-18OERSVFVT2>
2021-09-18 08:51:39 -0600

    Removed timing information, cleaned up unneeded comments, removed unneeded extended sleep.

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,5 @@
 #include <windows.h>
 #include <tlhelp32.h>
-#include <chrono>
 #include <string>
 
 int main() {
@@ -8,20 +7,13 @@ int main() {
 
 	while (true) {
 		bool wasRTXVoiceProcessFound = false;
-		//Time how long it takes to get all our processes
-		auto start = std::chrono::high_resolution_clock::now();
-
-		//Get a handle to all processes in the system snapshot.
 		HANDLE snapshotHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-		//Make sure we got a valid snapshot handle
 		if (snapshotHandle != INVALID_HANDLE_VALUE) {
 			PROCESSENTRY32 processEntry = {};
 			processEntry.dwSize = sizeof(PROCESSENTRY32);
-
 			if (Process32First(snapshotHandle, &processEntry)) {
 				wasRTXVoiceProcessFound = (!RTXVoiceProcessName.compare(processEntry.szExeFile));
 			}
-
 			while (!wasRTXVoiceProcessFound && Process32Next(snapshotHandle, &processEntry)) {
 				wasRTXVoiceProcessFound = (!RTXVoiceProcessName.compare(processEntry.szExeFile));
 			}
@@ -32,13 +24,8 @@ int main() {
 					NULL,
 					L"C:/Program Files/NVIDIA Corporation/NVIDIA RTX Voice/",
 					SW_MINIMIZE);
-
-				Sleep(10000);
 			}
 		}
-		//Finish the timing after we've performed the tasks.
-		auto end = std::chrono::high_resolution_clock::now();
-		auto timeElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 		Sleep(5000);
 	}
 	return 0;
